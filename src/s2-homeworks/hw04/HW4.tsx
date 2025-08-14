fbb854884918e2cd93fa7cde85e873ae780a75b3
wewealka <wewepoweralka@mail.ru>
2025-08-14 15:45:48 +0300

    feat: hw-4

--- src/s2-homeworks/hw04/HW4.tsx
+++ src/s2-homeworks/hw04/HW4.tsx
@@ -13,7 +13,6 @@ const HW4 = () => {
     return (
         <div id={'hw4'}>
             <div className={s2.hwTitle}>Homework #4</div>
-            {/*демонстрация возможностей компонент:*/}
             <div className={s2.hw}>
                 <Stand />
             </div>
