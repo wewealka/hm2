4020546eccb9cb43ef51d04da01daf52d3308ebe
wewealka <wewepoweralka@mail.ru>
2025-09-13 17:19:07 +0300

    Complete HW5: routing, active states, and styling

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -4,14 +4,16 @@ import HW1 from "../s2-homeworks/hw01/HW1";
 import HW2 from "../s2-homeworks/hw02/HW2";
 import HW3 from "../s2-homeworks/hw03/HW3";
 import HW4 from "../s2-homeworks/hw04/HW4";
+import HW5 from "../s2-homeworks/hw05/HW5";
 
 function App() {
   return (
     <div className={s.App}>
-      <HW1 />
+      {/* <HW1 />
       <HW2 />
       <HW3 />
-      <HW4 />
+      <HW4 /> */}
+      <HW5/>
     </div>
   );
 }
